3ea9055be5819708d3b81d9309ca517009cd5c9b
app2641 <app2641@gmail.com>
2014-06-25 17:15:45 +0900

    みんなが抜いたエロ動画プラグインの実装 #12
    rssテストデータの修正 #12

--- tests/Midnight/Crawler/UriManagerTest.php
+++ tests/Midnight/Crawler/UriManagerTest.php
@@ -68,6 +68,31 @@ class UriManagerTest extends PHPUnit_Framework_TestCase
     /**
      * @test
      * @group uri
+     * @group uri-asg
+     */
+    public function asgのurlを整形する場合 ()
+    {
+        $url = 'http://asg.to?mcd=fogefoge&blog=apppppp';
+        $url = $this->manager->resolve($url);
+        $this->assertEquals('http://asg.to/contentsPage.html?mcd=fogefoge', $url);
+
+        $url = 'http://asg.to/contents/embed?browser=chrome&mcd=barbar';
+        $url = $this->manager->resolve($url);
+        $this->assertEquals('http://asg.to/contentsPage.html?mcd=barbar', $url);
+
+        $url = 'http://asg.to/search?q=piyo';
+        $url = $this->manager->resolve($url);
+        $this->assertEquals('http://asg.to', $url);
+
+        $url = 'http://asg.to/about';
+        $url = $this->manager->resolve($url);
+        $this->assertEquals('http://asg.to', $url);
+    }
+
+
+    /**
+     * @test
+     * @group uri
      * @group uri-unrelated-url
      */
     public function AdultMidnightに関係のないurlの場合 ()
