0ec4251da79075038756909bcbd67e543cc6203f
app2641 <app2641@gmail.com>
2014-07-11 17:18:37 +0900

    Matomeプラグインの実装 #24

--- tests/Midnight/Factory/CrawlerPluginFactoryTest.php
+++ tests/Midnight/Factory/CrawlerPluginFactoryTest.php
@@ -118,6 +118,18 @@ class CrawlerPluginFactoryTest extends PHPUnit_Framework_TestCase
     /**
      * @test
      * @group p_factory
+     * @group p_factory-get-matome
+     */
+    public function Matomeプラグインの取得 ()
+    {
+        $plugin = $this->container->get('Matome');
+        $this->assertInstanceOf('Midnight\Crawler\Plugin\Matome', $plugin);
+    }
+
+
+    /**
+     * @test
+     * @group p_factory
      * @group p_factory-get-minna
      */
     public function Minnaプラグインの取得 ()
