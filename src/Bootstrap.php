85b9de117a5e2397e0ddf6eab19389ecf3208f2c
app2641 <app2641@gmail.com>
2014-09-03 18:18:33 +0900

    定数の修正 #32

--- src/Bootstrap.php
+++ src/Bootstrap.php
@@ -4,8 +4,8 @@ defined('ROOT') || define('ROOT', realpath(__DIR__.'/../'));
 defined('LIB') || define('LIB', ROOT.'/src');
 defined('APP') || define('APP', 'Midnight');
 
-//$is_ec2 = (is_dir('/home/ec2-user')) ? true: false;
-$is_ec2 = (is_dir('/home/ec2-user')) ? true: true;
+$is_ec2 = (is_dir('/home/ec2-user')) ? true: false;
+//$is_ec2 = (is_dir('/home/ec2-user')) ? true: true;
 defined('IS_EC2') || define('IS_EC2', $is_ec2);
 
 $loader = require_once ROOT.'/vendor/autoload.php';
